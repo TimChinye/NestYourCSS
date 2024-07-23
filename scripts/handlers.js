b0521f7daed2d0bc71b7c41e2c9722bfa07a562a
timchinye123 <timchinye123@gmail.com>
2024-07-23 17:17:26 +0000

    Front-end done (to a satisfactory level)

---
+++ scripts/handlers.js
@@ -0,0 +1,52 @@
+document.body.addEventListener('mousemove', (e) => {
+  requestAnimationFrame(() => {
+    window.cursorX = e.clientX;
+    window.cursorY = e.clientY;
+
+    const target = e.target;
+
+    const socialButton = target.classList.contains('socialButton') ? target : target.closest('.socialButton');
+    if (socialButton) {
+      repositionButtonBG(socialButton);
+    };
+    
+    if (target === splashTextElem) {
+      attemptSplashTextUpdate();
+    }
+    
+    if (target === lineNumbers.parentElement) {
+      onFirstView(e);
+    }
+    
+    let editorSection = document.getElementsByTagName('main')[0];
+    if (e.pageY > editorSection.offsetTop - editorSection.offsetHeight && e.pageY < editorSection.offsetTop + (editorSection.offsetHeight * 2)) {
+      updateActiveLine();
+    }
+    
+    let mainSection = document.getElementById('groupingStylesTogether');
+    if (e.pageY > 0 && e.pageY < mainSection.offsetTop + (mainSection.offsetHeight * 2)) {
+      moveMainBackground();
+    }
+    
+    let nycssBadge = document.getElementById('nycssBadge');
+    if (nycssBadge.className == 'hover-animation') {
+      console.log("test");
+      debounce(moveCursorBackground, 100)();
+    }
+  });
+});
+
+window.addEventListener('scroll', (e) => {
+  requestAnimationFrame(() => {
+    window.smoothScrollX = window.scrollX || window.pageXOffset;
+    window.smoothScrollY = window.scrollY || window.pageYOffset;
+
+    updateLogoState();
+  });
+});
+
+document.addEventListener('click', (e) => {
+  requestAnimationFrame(() => {
+    moveCursorBackground();
+  });
+});

360daee43a3f9b7603913365b83c88ddf4ab98f7
timchinye123 <timchinye123@gmail.com>
2024-08-30 19:52:07 +0000

    Synced with quickly + updated internal nesting functionality

--- scripts/main.js
+++ scripts/main.js
@@ -15,3 +15,20 @@ const roundNumber = (num, dp = 2) => {
 };
 
 const clone = (obj) => JSON.parse(JSON.stringify(obj));
+
+// JSON.stringify but also avoids circular references
+function safeStringify(obj) {
+    const seen = new WeakSet();
+    return JSON.stringify(obj, (key, value) => {
+        if (typeof value === "object" && value !== null) {
+            if (seen.has(value)) {
+                return;
+            }
+            seen.add(value);
+        }
+        return value;
+    });
+}
+
+// Prevents chrome's console for logging references of arrays
+console.log = ((logFunc) => (...args) => logFunc(...args.map(arg => JSON.parse(safeStringify(arg)))))(console.log);

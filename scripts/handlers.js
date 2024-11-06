2825c1cfe09ae30c7a2d854c9fff37d281453068
timchinye123 <timchinye123@gmail.com>
2024-11-06 02:08:32 +0000

    patch

--- scripts/handlers.js
+++ scripts/handlers.js
@@ -326,7 +326,7 @@ function comboHandler(inputElem, close) {
   
   switch (labelElem.id) {
     case "externalCss": {
-      fetch('https://cors-anywhere.herokuapp.com/' + inputElem.innerHTML).then((response) => {
+      fetch('https://proxy.cors.sh/' + inputElem.innerHTML).then((response) => {
           if (!response.ok) {
               throw new Error(`HTTP error! Status: ${response.status}`);
           }

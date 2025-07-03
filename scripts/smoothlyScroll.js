493f24fd264b5f16ff502c2418c4a491848edeb0
timchinye123 <timchinye123@gmail.com>
2025-07-03 11:35:03 +0000

    Fixed major bug - can now scroll the settings, and not scroll the page in nesting mode

--- scripts/smoothlyScroll.js
+++ scripts/smoothlyScroll.js
@@ -1,32 +1,57 @@
-const lenis = new Lenis({
-    wrapper: scrollWrapper,
-    autoResize: true 
-})
+let currentLenis;
+
+const mainSettings = scrollWrapper.querySelector('#mainSettings');
+const mainElement = scrollWrapper.firstElementChild;
+var isNesting = mainElement.classList.contains('nesting');
+
+
+const updateLenisTarget = () => {
+  const target = isNesting ? mainSettings : scrollWrapper;
+
+  if (currentLenis) currentLenis.destroy();
+
+  currentLenis = new Lenis({
+      wrapper: target,
+      autoResize: true
+  });
+};
+
+const observer = new MutationObserver(() => {
+  const isCurrentlyNesting = mainElement.classList.contains('nesting');
+
+  if (isCurrentlyNesting !== isNesting) {
+    isNesting = isCurrentlyNesting;
+
+    updateLenisTarget();
+  }
+});
+
+updateLenisTarget();
+observer.observe(mainElement, { attributes: true, attributeFilter: ['class'] });
 
 function raf(time) {
-  lenis.raf(time)
+  currentLenis?.raf(time)
   requestAnimationFrame(raf)
 }
-
 requestAnimationFrame(raf)
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
 
-        const targetSelector = this.getAttribute('href');
-        let targetElement;
+    const targetSelector = this.getAttribute('href');
+    let targetElement;
 
-        if (targetSelector == '#') targetElement = scrollWrapper.firstElementChild;
-        else targetElement = document.getElementById(targetSelector.slice(1));
-        
-        if (lenis) lenis.scrollTo(targetElement, {
-          duration: 1.5,
-          lock: true
-        });
-        else scrollWrapper.scrollTo({
-          top: targetElement.offsetTop,
-          behavior: 'smooth'
-        });
+    if (targetSelector == '#') targetElement = scrollWrapper.firstElementChild;
+    else targetElement = document.getElementById(targetSelector.slice(1));
+    
+    if (currentLenis) currentLenis.scrollTo(targetElement, {
+      duration: 1.5,
+      lock: true
     });
+    else scrollWrapper.scrollTo({
+      top: targetElement.offsetTop,
+      behavior: 'smooth'
+    });
+  });
 });

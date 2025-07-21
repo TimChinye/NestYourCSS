77c5381fbf736151f57ce748c47c2af6c2cfc9fc
timchinye123 <timchinye123@gmail.com>
2025-07-21 20:10:42 +0000

    Fixed all animation bugs, improved accessibility

--- scripts/minieditor.js
+++ scripts/minieditor.js
@@ -1,38 +1,45 @@
-let lines = lineNumbers.nextElementSibling.textContent.split("\n");
-lineNumbers.nextElementSibling.innerHTML = lines.map((item) => {
-  if (!item.trim()) return '<br>';
 
-  let line = item.trim().replace(/{TAB}/g, "&emsp;").replace(/{BOLD} /g, "<b>").replace(/ {\/BOLD}/g, "</b>");
+let lineNumbers = miniEditor.querySelector('#lineNumbers');
+let nycssCodeExample = miniEditor.querySelector('#nycssCodeExample');
+
+let lines = nycssCodeExample.textContent.trim().split("\n").concat('\n');
+lineNumbers.innerHTML = [...lines].fill().map((_, i) => `<div>${i + 1}</div>`).join("");
+nycssCodeExample.innerHTML = lines.map((item) => {
+  if (!item.trim()) return '<br>';
+  
+  let line = item.trim().replace(/{TAB}/g, "&emsp;").replace(/{BOLD} /g, "<strong>").replace(/ {\/BOLD}/g, "</strong>");
   
   let splitLine = line.split(/(&emsp; )+/);
   let lineContent = splitLine[splitLine.length - 1];
   
   // Only wrap non-tag characters in spans
-  line = line.slice(0, line.length - lineContent.length) + lineContent.split(/(<\/?b>)/).reduce((acc, part, index, arr) => acc + (part.match(/(<\/?b>)/) ? part : [...part].map((ch, i) => `<span style="--charIndex: ${arr.slice(0, index).filter((p) => !p.match(/(<\/?b>)/)).join("").length + i}">${ch}</span>`).join("")), "");
+  line = line.slice(0, line.length - lineContent.length) + lineContent.split(/(<\/?strong>)/).reduce((acc, part, index, arr) => acc + (part.match(/(<\/?strong>)/) ? part : [...part].map((ch, i) => `<span style="--charIndex: ${arr.slice(0, index).filter((p) => !p.match(/(<\/?strong>)/)).join("").length + i}">${ch}</span>`).join("")), "");
   
   return `<div>${line}</div>`;
 }).join("");
-
-lineNumbers.innerHTML = lines.fill().map((item, i) => `<div>${i + 1}</div>`).join("");
+nycssCodeExample.parentElement.innerHTML = nycssCodeExample.outerHTML.trim();
 
 /* Set active line */
 
 function updateActiveLine(cursorX, cursorY) {
+  let lineNumbers = miniEditor.querySelector('#lineNumbers');
+  let nycssCodeExample = miniEditor.querySelector('#nycssCodeExample');
+
   /* Vertical */
   
   debounce(() => {
-    const segmentHeight = lineNumbers.parentElement.parentElement.offsetHeight / lines.length;
+    const segmentHeight = miniEditor.parentElement.offsetHeight / lines.length;
     
     const activeLine = Math.ceil(cursorY / segmentHeight);
     
-    lineNumbers.nextElementSibling.style.setProperty('--activeLine', activeLine + '');
-    lineNumbers.nextElementSibling.style.setProperty('--maxLines', lines.length + '');
+    nycssCodeExample.parentElement.style.setProperty('--activeLine', activeLine + '');
+    nycssCodeExample.parentElement.style.setProperty('--maxLines', lines.length + '');
   }, 250)();
   
   /* Horizontal */
 
   let horizontalPos = cursorX / document.body.clientWidth;
-  let padding = lineNumbers.nextElementSibling.offsetLeft / lineNumbers.parentElement.offsetWidth;
+  let padding = nycssCodeExample.parentElement.offsetLeft / miniEditor.offsetWidth;
   let horizontalRePos = ((1 - (padding * 2)) * horizontalPos) + padding;
-  lineNumbers.nextElementSibling.style.setProperty('--intensityPos', roundNumber(horizontalRePos * 100) + '%');
+  nycssCodeExample.parentElement.style.setProperty('--intensityPos', roundNumber(horizontalRePos * 100) + '%');
 }
